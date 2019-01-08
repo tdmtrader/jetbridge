YoussB/concourse

topgun & ci: adds bbr tests
This commit adds two tests for testing the BBR configuration that
Concourse has.

It introduces two tests:

1. a test that verifies whether backing up and restoring works when
starting from a clean state and restoring back to that same state; and

2. another that checks if we can go back to a clean state after having a
pipeline set.

The first test consistently passes, while the second fails - to be fixed
in upcoming commits.

note.: we added an `ops` file that makes the postgres version static so
BBR doesn't fail when parsing the version of the latest postgres (see
[1]).

[1]: https://github.com/cloudfoundry-incubator/backup-and-restore-sdk-release/issues/14

concourse/concourse#2332

Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>
Co-authored-by: Bishoy youssef <byoussef@pivotal.io>

topgun/bbr_test.go
@@ -1,0 +1,115 @@
+package topgun_test
+
+import (
+	. "github.com/concourse/concourse/topgun"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+var _ = Describe("BBR", func() {
+	var atcs []boshInstance
+	var atc0URL string
+
+	BeforeEach(func() {
+		Deploy(
+			"deployments/concourse.yml",
+			"-o", "operations/bbr.yml",
+		)
+
+		waitForRunningWorker()
+
+		atcs = JobInstances("web")
+		atc0URL = "http://" + atcs[0].IP + ":8080"
+
+		fly.Login(atcUsername, atcPassword, atc0URL)
+	})
+
+	FContext("backing up a fresh deployment", func() {
+		var tmpDir string
+
+		BeforeEach(func() {
+			var err error
+			tmpDir, err = ioutil.TempDir("", "")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			os.RemoveAll(tmpDir)
+		})
+
+		It("backups and restores", func() {
+			backupArgs := []string{
+				"deployment",
+				"-d", deploymentName,
+				"backup",
+				"--artifact-path", tmpDir,
+			}
+
+			Run(nil, "bbr", backupArgs...)
+
+			entries, err := ioutil.ReadDir(tmpDir)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(entries).To(HaveLen(1))
+
+			restoreArgs := []string{
+				"deployment",
+				"-d", deploymentName,
+				"restore",
+				"--artifact-path", path.Join(tmpDir, entries[0].Name()),
+			}
+
+			Run(nil, "bbr", restoreArgs...)
+		})
+	})
+
+	// TODO - fix BBR first
+	XContext("restoring a deployment with data to the fresh state", func() {
+		var tmpDir string
+
+		BeforeEach(func() {
+			var err error
+			tmpDir, err = ioutil.TempDir("", "")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			os.RemoveAll(tmpDir)
+		})
+
+		It("backups and restores", func() {
+			backupArgs := []string{
+				"deployment",
+				"-d", deploymentName,
+				"backup",
+				"--artifact-path", tmpDir,
+			}
+
+			Run(nil, "bbr", backupArgs...)
+
+			entries, err := ioutil.ReadDir(tmpDir)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(entries).To(HaveLen(1))
+
+			fly.Run("set-pipeline", "-n", "-p", "pipeline", "-c", "./pipelines/get-task.yml")
+			pipelines := fly.GetPipelines()
+			Expect(pipelines).ToNot(BeEmpty())
+			Expect(pipelines[0].Name).To(Equal("pipeline"))
+
+			restoreArgs := []string{
+				"deployment",
+				"-d", deploymentName,
+				"restore",
+				"--artifact-path", path.Join(tmpDir, entries[0].Name()),
+			}
+
+			Run(nil, "bbr", restoreArgs...)
+
+			pipelines = fly.GetPipelines()
+			Expect(pipelines).To(BeEmpty())
+		})
+	})
+
+})

tdmtrader/jetbridge

topgun/bbr_test.go
@@ -1,0 +1,116 @@
+package topgun_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+
+	. "github.com/concourse/concourse/topgun"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("BBR", func() {
+	var atcs []boshInstance
+	var atc0URL string
+
+	BeforeEach(func() {
+		Deploy(
+			"deployments/concourse.yml",
+			"-o", "operations/bbr.yml",
+		)
+
+		waitForRunningWorker()
+
+		atcs = JobInstances("web")
+		atc0URL = "http://" + atcs[0].IP + ":8080"
+
+		fly.Login(atcUsername, atcPassword, atc0URL)
+	})
+
+	Context("backing up a fresh deployment", func() {
+		var tmpDir string
+
+		BeforeEach(func() {
+			var err error
+			tmpDir, err = ioutil.TempDir("", "")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			os.RemoveAll(tmpDir)
+		})
+
+		It("backups and restores", func() {
+			backupArgs := []string{
+				"deployment",
+				"-d", deploymentName,
+				"backup",
+				"--artifact-path", tmpDir,
+			}
+
+			Run(nil, "bbr", backupArgs...)
+
+			entries, err := ioutil.ReadDir(tmpDir)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(entries).To(HaveLen(1))
+
+			restoreArgs := []string{
+				"deployment",
+				"-d", deploymentName,
+				"restore",
+				"--artifact-path", path.Join(tmpDir, entries[0].Name()),
+			}
+
+			Run(nil, "bbr", restoreArgs...)
+		})
+	})
+
+	// TODO - fix BBR first
+	XContext("restoring a deployment with data to the fresh state", func() {
+		var tmpDir string
+
+		BeforeEach(func() {
+			var err error
+			tmpDir, err = ioutil.TempDir("", "")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			os.RemoveAll(tmpDir)
+		})
+
+		It("backups and restores", func() {
+			backupArgs := []string{
+				"deployment",
+				"-d", deploymentName,
+				"backup",
+				"--artifact-path", tmpDir,
+			}
+
+			Run(nil, "bbr", backupArgs...)
+
+			entries, err := ioutil.ReadDir(tmpDir)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(entries).To(HaveLen(1))
+
+			fly.Run("set-pipeline", "-n", "-p", "pipeline", "-c", "./pipelines/get-task.yml")
+			pipelines := fly.GetPipelines()
+			Expect(pipelines).ToNot(BeEmpty())
+			Expect(pipelines[0].Name).To(Equal("pipeline"))
+
+			restoreArgs := []string{
+				"deployment",
+				"-d", deploymentName,
+				"restore",
+				"--artifact-path", path.Join(tmpDir, entries[0].Name()),
+			}
+
+			Run(nil, "bbr", restoreArgs...)
+
+			pipelines = fly.GetPipelines()
+			Expect(pipelines).To(BeEmpty())
+		})
+	})
+
+})