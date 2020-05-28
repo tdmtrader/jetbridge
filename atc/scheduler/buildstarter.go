zhang1993/concourse

atc: structure: BuildFactory -> BuildPlanner
...and rewrite the test suite.

I found myself spending way too much time examining the tests in the
package whenever I made a seemingly simple change to the Plan.

This package is largely a mechanical task of making sure the right
PlanConfig turns into the write Plan, so it's a good fit for
table-driven tests. This should hopefully be much lower maintenance.

The tests still assert that plan IDs are unique, and assert on exact
plan IDs when necessary. Otherwise they are omitted to reduce noise and
make the tests a bit easier to write.

The precedence between attempts:, timeout:, and the various hooks is
also covered.

Signed-off-by: Alex Suraci <asuraci@pivotal.io>

atc/scheduler/buildstarter.go
@@ -24,10 +24,10 @@ type BuildStarter interface {
 	) (bool, error)
 }
 
-//go:generate counterfeiter . BuildFactory
+//go:generate counterfeiter . BuildPlanner
 
-type BuildFactory interface {
-	Create(atc.JobConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
+type BuildPlanner interface {
+	Create(atc.PlanConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
 }
 
 type Build interface {
@@ -38,17 +38,17 @@ type Build interface {
 }
 
 func NewBuildStarter(
-	factory BuildFactory,
+	planner BuildPlanner,
 	algorithm Algorithm,
 ) BuildStarter {
 	return &buildStarter{
-		factory:   factory,
+		planner:   planner,
 		algorithm: algorithm,
 	}
 }
 
 type buildStarter struct {
-	factory   BuildFactory
+	planner   BuildPlanner
 	algorithm Algorithm
 }
 
@@ -235,7 +235,7 @@ func (s *buildStarter) tryStartNextPendingBuild(
 		return startResults{}, fmt.Errorf("config: %w", err)
 	}
 
-	plan, err := s.factory.Create(config, resourceConfigs, resourceTypes.Deserialize(), buildInputs)
+	plan, err := s.planner.Create(config.Plan(), resourceConfigs, resourceTypes.Deserialize(), buildInputs)
 	if err != nil {
 		logger.Error("failed-to-create-build-plan", err)
 

atc/scheduler/factory/factoryfakes/fake_build_factory.go
@@ -10,10 +10,10 @@ import (
 )
 
 type FakeBuildFactory struct {
-	CreateStub        func(atc.JobConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
+	CreateStub        func(atc.PlanConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
 	createMutex       sync.RWMutex
 	createArgsForCall []struct {
-		arg1 atc.JobConfig
+		arg1 atc.PlanConfig
 		arg2 atc.ResourceConfigs
 		arg3 atc.VersionedResourceTypes
 		arg4 []db.BuildInput
@@ -30,7 +30,7 @@ type FakeBuildFactory struct {
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeBuildFactory) Create(arg1 atc.JobConfig, arg2 atc.ResourceConfigs, arg3 atc.VersionedResourceTypes, arg4 []db.BuildInput) (atc.Plan, error) {
+func (fake *FakeBuildFactory) Create(arg1 atc.PlanConfig, arg2 atc.ResourceConfigs, arg3 atc.VersionedResourceTypes, arg4 []db.BuildInput) (atc.Plan, error) {
 	var arg4Copy []db.BuildInput
 	if arg4 != nil {
 		arg4Copy = make([]db.BuildInput, len(arg4))
@@ -39,7 +39,7 @@ func (fake *FakeBuildFactory) Create(arg1 atc.JobConfig, arg2 atc.ResourceConfig
 	fake.createMutex.Lock()
 	ret, specificReturn := fake.createReturnsOnCall[len(fake.createArgsForCall)]
 	fake.createArgsForCall = append(fake.createArgsForCall, struct {
-		arg1 atc.JobConfig
+		arg1 atc.PlanConfig
 		arg2 atc.ResourceConfigs
 		arg3 atc.VersionedResourceTypes
 		arg4 []db.BuildInput
@@ -62,13 +62,13 @@ func (fake *FakeBuildFactory) CreateCallCount() int {
 	return len(fake.createArgsForCall)
 }
 
-func (fake *FakeBuildFactory) CreateCalls(stub func(atc.JobConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)) {
+func (fake *FakeBuildFactory) CreateCalls(stub func(atc.PlanConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)) {
 	fake.createMutex.Lock()
 	defer fake.createMutex.Unlock()
 	fake.CreateStub = stub
 }
 
-func (fake *FakeBuildFactory) CreateArgsForCall(i int) (atc.JobConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) {
+func (fake *FakeBuildFactory) CreateArgsForCall(i int) (atc.PlanConfig, atc.ResourceConfigs, atc.VersionedResourceTypes, []db.BuildInput) {
 	fake.createMutex.RLock()
 	defer fake.createMutex.RUnlock()
 	argsForCall := fake.createArgsForCall[i]

tdmtrader/jetbridge

atc/scheduler/buildstarter.go
@@ -20,10 +20,10 @@ type BuildStarter interface {
 	) (bool, error)
 }
 
-//go:generate counterfeiter . BuildFactory
+//go:generate counterfeiter . BuildPlanner
 
-type BuildFactory interface {
-	Create(atc.JobConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
+type BuildPlanner interface {
+	Create(atc.PlanConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
 }
 
 type Build interface {
@@ -34,17 +34,17 @@ type Build interface {
 }
 
 func NewBuildStarter(
-	factory BuildFactory,
+	planner BuildPlanner,
 	algorithm Algorithm,
 ) BuildStarter {
 	return &buildStarter{
-		factory:   factory,
+		planner:   planner,
 		algorithm: algorithm,
 	}
 }
 
 type buildStarter struct {
-	factory   BuildFactory
+	planner   BuildPlanner
 	algorithm Algorithm
 }
 
@@ -198,7 +198,7 @@ func (s *buildStarter) tryStartNextPendingBuild(
 		return startResults{}, fmt.Errorf("config: %w", err)
 	}
 
-	plan, err := s.factory.Create(config, job.Resources, job.ResourceTypes, buildInputs)
+	plan, err := s.planner.Create(config.Plan(), job.Resources, job.ResourceTypes, buildInputs)
 	if err != nil {
 		logger.Error("failed-to-create-build-plan", err)
 

atc/scheduler/factory/factoryfakes/fake_build_factory.go
@@ -10,10 +10,10 @@ import (
 )
 
 type FakeBuildFactory struct {
-	CreateStub        func(atc.JobConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
+	CreateStub        func(atc.PlanConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)
 	createMutex       sync.RWMutex
 	createArgsForCall []struct {
-		arg1 atc.JobConfig
+		arg1 atc.PlanConfig
 		arg2 db.SchedulerResources
 		arg3 atc.VersionedResourceTypes
 		arg4 []db.BuildInput
@@ -30,7 +30,7 @@ type FakeBuildFactory struct {
 	invocationsMutex sync.RWMutex
 }
 
-func (fake *FakeBuildFactory) Create(arg1 atc.JobConfig, arg2 db.SchedulerResources, arg3 atc.VersionedResourceTypes, arg4 []db.BuildInput) (atc.Plan, error) {
+func (fake *FakeBuildFactory) Create(arg1 atc.PlanConfig, arg2 db.SchedulerResources, arg3 atc.VersionedResourceTypes, arg4 []db.BuildInput) (atc.Plan, error) {
 	var arg4Copy []db.BuildInput
 	if arg4 != nil {
 		arg4Copy = make([]db.BuildInput, len(arg4))
@@ -39,7 +39,7 @@ func (fake *FakeBuildFactory) Create(arg1 atc.JobConfig, arg2 db.SchedulerResour
 	fake.createMutex.Lock()
 	ret, specificReturn := fake.createReturnsOnCall[len(fake.createArgsForCall)]
 	fake.createArgsForCall = append(fake.createArgsForCall, struct {
-		arg1 atc.JobConfig
+		arg1 atc.PlanConfig
 		arg2 db.SchedulerResources
 		arg3 atc.VersionedResourceTypes
 		arg4 []db.BuildInput
@@ -62,13 +62,13 @@ func (fake *FakeBuildFactory) CreateCallCount() int {
 	return len(fake.createArgsForCall)
 }
 
-func (fake *FakeBuildFactory) CreateCalls(stub func(atc.JobConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)) {
+func (fake *FakeBuildFactory) CreateCalls(stub func(atc.PlanConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) (atc.Plan, error)) {
 	fake.createMutex.Lock()
 	defer fake.createMutex.Unlock()
 	fake.CreateStub = stub
 }
 
-func (fake *FakeBuildFactory) CreateArgsForCall(i int) (atc.JobConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) {
+func (fake *FakeBuildFactory) CreateArgsForCall(i int) (atc.PlanConfig, db.SchedulerResources, atc.VersionedResourceTypes, []db.BuildInput) {
 	fake.createMutex.RLock()
 	defer fake.createMutex.RUnlock()
 	argsForCall := fake.createArgsForCall[i]