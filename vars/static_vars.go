concourse/concourse

vars: structure: allow converting KV pairs to/from static vars
concourse/concourse#6041

Adds a new KVPairs type that is a list of key-value pairs. KVPairs can
be Expanded into a StaticVariables (which supports the usual Get/List
methods of a Variables impl), and StaticVariables can be Flattened into
a KVPairs.

The intention of Expand is to go from a list of var flags to a
StaticVariables for interpolating the pipeline template. The intention
of Flatten is to convert a pipeline's instance vars to a flat string
form using dot notation.

Note that we already have an implementation of both methods in
atc/pipeline.go for instance vars - however, I intend to use this
simplified implementation that does not traverse lists since the
notation for doing so is ambiguous (if a field name is an integer, it
assumes that it's an index in a list). Instead of using dot notation for
lists, we'll just display the list as '[a, b, c]'

Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

vars/static_vars.go
@@ -57,3 +57,78 @@ func Traverse(val interface{}, name string, fields []string) (interface{}, error
 	}
 	return val, nil
 }
+
+func (v StaticVariables) Flatten() KVPairs {
+	var flat KVPairs
+	for k, vv := range v {
+		flat = append(flat, flatten(k, nil, vv)...)
+	}
+	return flat
+}
+
+func flatten(path string, fields []string, value interface{}) KVPairs {
+	var flat KVPairs
+
+	switch node := value.(type) {
+	case map[string]interface{}:
+		for k, v := range node {
+			flat = append(flat, flatten(path, append(fields, k), v)...)
+		}
+	case map[interface{}]interface{}:
+		for k, v := range node {
+			if str, ok := k.(string); ok {
+				flat = append(flat, flatten(path, append(fields, str), v)...)
+			}
+		}
+	default:
+		flat = KVPairs{{
+			Ref: Reference{
+				Path:   path,
+				Fields: fields,
+			},
+			Value: value,
+		}}
+	}
+
+	return flat
+}
+
+type KVPair struct {
+	Ref   Reference
+	Value interface{}
+}
+
+type KVPairs []KVPair
+
+func (f KVPairs) Expand() StaticVariables {
+	out := map[string]interface{}{}
+	for _, pair := range f {
+		upsert(out, pair.Ref.Path, pair.Ref.Fields, pair.Value)
+	}
+	return out
+}
+
+func upsert(out map[string]interface{}, path string, fields []string, value interface{}) {
+	node, ok := out[path]
+	if !ok {
+		out[path] = constructValue(fields, value)
+		return
+	}
+	nodeMap, ok := node.(map[string]interface{})
+	if !ok {
+		out[path] = constructValue(fields, value)
+		return
+	}
+	if len(fields) == 0 {
+		out[path] = value
+		return
+	}
+	upsert(nodeMap, fields[0], fields[1:], value)
+}
+
+func constructValue(fields []string, value interface{}) interface{} {
+	if len(fields) == 0 {
+		return value
+	}
+	return constructValue(fields[:len(fields)-1], map[string]interface{}{fields[len(fields)-1]: value})
+}

vars/static_vars_test.go
@@ -86,4 +86,88 @@ var _ = Describe("StaticVariables", func() {
 			Expect(err).ToNot(HaveOccurred())
 		})
 	})
+
+	Describe("Flatten", func() {
+		for _, tt := range []struct {
+			desc     string
+			expanded StaticVariables
+			kvPairs  KVPairs
+		}{
+			{
+				desc: "flattens recursively",
+				expanded: StaticVariables{
+					"hello": "world",
+					"foo":   map[string]interface{}{"bar": "baz", "abc": map[interface{}]interface{}{"def": "ghi", "jkl": "mno"}},
+				},
+				kvPairs: KVPairs{
+					{Ref: Reference{Path: "hello"}, Value: "world"},
+					{Ref: Reference{Path: "foo", Fields: []string{"bar"}}, Value: "baz"},
+					{Ref: Reference{Path: "foo", Fields: []string{"abc", "def"}}, Value: "ghi"},
+					{Ref: Reference{Path: "foo", Fields: []string{"abc", "jkl"}}, Value: "mno"},
+				},
+			},
+		} {
+			tt := tt
+			It(tt.desc, func() {
+				Expect(tt.expanded.Flatten()).To(ConsistOf(tt.kvPairs))
+			})
+		}
+	})
+
+	Describe("Expand", func() {
+		for _, tt := range []struct {
+			desc     string
+			kvPairs  KVPairs
+			expanded StaticVariables
+		}{
+			{
+				desc: "merges flat elements into map",
+				kvPairs: KVPairs{
+					{Ref: Reference{Path: "hello"}, Value: "world"},
+					{Ref: Reference{Path: "foo"}, Value: map[string]interface{}{"bar": "baz"}},
+				},
+				expanded: StaticVariables{
+					"hello": "world",
+					"foo":   map[string]interface{}{"bar": "baz"},
+				},
+			},
+			{
+				desc: "merges recurses through fields",
+				kvPairs: KVPairs{
+					{Ref: Reference{Path: "hello", Fields: []string{"a", "b"}}, Value: "world"},
+					{Ref: Reference{Path: "foo"}, Value: map[string]interface{}{"bar": map[string]interface{}{"abc": "def"}}},
+					{Ref: Reference{Path: "foo", Fields: []string{"bar", "ghi"}}, Value: "jkl"},
+				},
+				expanded: StaticVariables{
+					"hello": map[string]interface{}{"a": map[string]interface{}{"b": "world"}},
+					"foo":   map[string]interface{}{"bar": map[string]interface{}{"abc": "def", "ghi": "jkl"}},
+				},
+			},
+			{
+				desc: "overwrites non-map nodes",
+				kvPairs: KVPairs{
+					{Ref: Reference{Path: "foo"}, Value: map[string]interface{}{"bar": "baz"}},
+					{Ref: Reference{Path: "foo", Fields: []string{"bar", "ghi"}}, Value: "jkl"},
+				},
+				expanded: StaticVariables{
+					"foo": map[string]interface{}{"bar": map[string]interface{}{"ghi": "jkl"}},
+				},
+			},
+			{
+				desc: "overwrites full nodes",
+				kvPairs: KVPairs{
+					{Ref: Reference{Path: "foo"}, Value: map[string]interface{}{"bar": "baz"}},
+					{Ref: Reference{Path: "foo"}, Value: "jkl"},
+				},
+				expanded: StaticVariables{
+					"foo": "jkl",
+				},
+			},
+		} {
+			tt := tt
+			It(tt.desc, func() {
+				Expect(tt.kvPairs.Expand()).To(Equal(tt.expanded))
+			})
+		}
+	})
 })

tdmtrader/jetbridge

vars/static_vars.go
@@ -18,3 +18,78 @@ func (v StaticVariables) List() ([]VariableDefinition, error) {
 
 	return defs, nil
 }
+
+func (v StaticVariables) Flatten() KVPairs {
+	var flat KVPairs
+	for k, vv := range v {
+		flat = append(flat, flatten(k, nil, vv)...)
+	}
+	return flat
+}
+
+func flatten(path string, fields []string, value interface{}) KVPairs {
+	var flat KVPairs
+
+	switch node := value.(type) {
+	case map[string]interface{}:
+		for k, v := range node {
+			flat = append(flat, flatten(path, append(fields, k), v)...)
+		}
+	case map[interface{}]interface{}:
+		for k, v := range node {
+			if str, ok := k.(string); ok {
+				flat = append(flat, flatten(path, append(fields, str), v)...)
+			}
+		}
+	default:
+		flat = KVPairs{{
+			Ref: VariableReference{
+				Path:   path,
+				Fields: fields,
+			},
+			Value: value,
+		}}
+	}
+
+	return flat
+}
+
+type KVPair struct {
+	Ref   VariableReference
+	Value interface{}
+}
+
+type KVPairs []KVPair
+
+func (f KVPairs) Expand() StaticVariables {
+	out := map[string]interface{}{}
+	for _, pair := range f {
+		upsert(out, pair.Ref.Path, pair.Ref.Fields, pair.Value)
+	}
+	return out
+}
+
+func upsert(out map[string]interface{}, path string, fields []string, value interface{}) {
+	node, ok := out[path]
+	if !ok {
+		out[path] = constructValue(fields, value)
+		return
+	}
+	nodeMap, ok := node.(map[string]interface{})
+	if !ok {
+		out[path] = constructValue(fields, value)
+		return
+	}
+	if len(fields) == 0 {
+		out[path] = value
+		return
+	}
+	upsert(nodeMap, fields[0], fields[1:], value)
+}
+
+func constructValue(fields []string, value interface{}) interface{} {
+	if len(fields) == 0 {
+		return value
+	}
+	return constructValue(fields[:len(fields)-1], map[string]interface{}{fields[len(fields)-1]: value})
+}

vars/static_vars_test.go
@@ -51,4 +51,88 @@ var _ = Describe("StaticVariables", func() {
 			Expect(err).ToNot(HaveOccurred())
 		})
 	})
+
+	Describe("Flatten", func() {
+		for _, tt := range []struct {
+			desc     string
+			expanded StaticVariables
+			kvPairs  KVPairs
+		}{
+			{
+				desc: "flattens recursively",
+				expanded: StaticVariables{
+					"hello": "world",
+					"foo":   map[string]interface{}{"bar": "baz", "abc": map[interface{}]interface{}{"def": "ghi", "jkl": "mno"}},
+				},
+				kvPairs: KVPairs{
+					{Ref: VariableReference{Path: "hello"}, Value: "world"},
+					{Ref: VariableReference{Path: "foo", Fields: []string{"bar"}}, Value: "baz"},
+					{Ref: VariableReference{Path: "foo", Fields: []string{"abc", "def"}}, Value: "ghi"},
+					{Ref: VariableReference{Path: "foo", Fields: []string{"abc", "jkl"}}, Value: "mno"},
+				},
+			},
+		} {
+			tt := tt
+			It(tt.desc, func() {
+				Expect(tt.expanded.Flatten()).To(ConsistOf(tt.kvPairs))
+			})
+		}
+	})
+
+	Describe("Expand", func() {
+		for _, tt := range []struct {
+			desc     string
+			kvPairs  KVPairs
+			expanded StaticVariables
+		}{
+			{
+				desc: "merges flat elements into map",
+				kvPairs: KVPairs{
+					{Ref: VariableReference{Path: "hello"}, Value: "world"},
+					{Ref: VariableReference{Path: "foo"}, Value: map[string]interface{}{"bar": "baz"}},
+				},
+				expanded: StaticVariables{
+					"hello": "world",
+					"foo":   map[string]interface{}{"bar": "baz"},
+				},
+			},
+			{
+				desc: "merges recurses through fields",
+				kvPairs: KVPairs{
+					{Ref: VariableReference{Path: "hello", Fields: []string{"a", "b"}}, Value: "world"},
+					{Ref: VariableReference{Path: "foo"}, Value: map[string]interface{}{"bar": map[string]interface{}{"abc": "def"}}},
+					{Ref: VariableReference{Path: "foo", Fields: []string{"bar", "ghi"}}, Value: "jkl"},
+				},
+				expanded: StaticVariables{
+					"hello": map[string]interface{}{"a": map[string]interface{}{"b": "world"}},
+					"foo":   map[string]interface{}{"bar": map[string]interface{}{"abc": "def", "ghi": "jkl"}},
+				},
+			},
+			{
+				desc: "overwrites non-map nodes",
+				kvPairs: KVPairs{
+					{Ref: VariableReference{Path: "foo"}, Value: map[string]interface{}{"bar": "baz"}},
+					{Ref: VariableReference{Path: "foo", Fields: []string{"bar", "ghi"}}, Value: "jkl"},
+				},
+				expanded: StaticVariables{
+					"foo": map[string]interface{}{"bar": map[string]interface{}{"ghi": "jkl"}},
+				},
+			},
+			{
+				desc: "overwrites full nodes",
+				kvPairs: KVPairs{
+					{Ref: VariableReference{Path: "foo"}, Value: map[string]interface{}{"bar": "baz"}},
+					{Ref: VariableReference{Path: "foo"}, Value: "jkl"},
+				},
+				expanded: StaticVariables{
+					"foo": "jkl",
+				},
+			},
+		} {
+			tt := tt
+			It(tt.desc, func() {
+				Expect(tt.kvPairs.Expand()).To(Equal(tt.expanded))
+			})
+		}
+	})
 })