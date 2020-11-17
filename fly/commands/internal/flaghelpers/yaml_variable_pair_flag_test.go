concourse/concourse

fly: behaviour: parse var flags as references
concourse/concourse#6041

...and use the references to properly construct a StaticVariables.

This also changes the YAMLVariableFlag slightly - rather than using
"gopkg.in/yaml.v2" to parse the YAML value, it uses "sigs.k8s.io/yaml",
which converts YAML to JSON first, and then unmarshals the JSON. This is
the package we use in most of Concourse. The reason for the change is
that parsing the YAML directly results in YAML objects being unmarshaled
as `map[interface{}]interface{}`, which is less than ideal. However, it
also means that numbers will be interpolated as `float64`, which is also
less than ideal - so, instead, we unmarshal numbers as `json.Number`,
which is just the string value of the number that's passed in.

We could possibly get rid of the map[interface{}]interface{} handling in
the vars package now, but I'm a bit worried about the implications of
that, so I'll hold off for now.

Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

fly/commands/internal/flaghelpers/yaml_variable_pair_flag_test.go
@@ -1,0 +1,65 @@
+package flaghelpers_test
+
+import (
+	"encoding/json"
+
+	"github.com/concourse/concourse/vars"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/concourse/concourse/fly/commands/internal/flaghelpers"
+)
+
+var _ = Describe("YAMLVariablePair", func() {
+	Describe("UnmarshalFlag", func() {
+		var variable *flaghelpers.YAMLVariablePairFlag
+
+		BeforeEach(func() {
+			variable = &flaghelpers.YAMLVariablePairFlag{}
+		})
+
+		for _, tt := range []struct {
+			desc string
+			flag string
+			ref  vars.Reference
+			val  interface{}
+			err  string
+		}{
+			{
+				desc: "basic",
+				flag: "key=value",
+				ref:  vars.Reference{Path: "key", Fields: []string{}},
+				val:  "value",
+			},
+			{
+				desc: "unmarshals arbitrary yaml",
+				flag: `key={hello:world: [a, b, c]}`,
+				ref:  vars.Reference{Path: `key`, Fields: []string{}},
+				val:  map[string]interface{}{"hello:world": []interface{}{"a", "b", "c"}},
+			},
+			{
+				desc: "unmarshals numbers as json.Number",
+				flag: `key={int: 1, float: 1.23}`,
+				ref:  vars.Reference{Path: `key`, Fields: []string{}},
+				val:  map[string]interface{}{"int": json.Number("1"), "float": json.Number("1.23")},
+			},
+			{
+				desc: "errors if yaml is malformed",
+				flag: `key={a: b`,
+				err:  `error converting YAML to JSON: yaml: line 1: did not find expected ',' or '}'`,
+			},
+		} {
+			tt := tt
+			It(tt.desc, func() {
+				err := variable.UnmarshalFlag(tt.flag)
+				if tt.err == "" {
+					Expect(err).ToNot(HaveOccurred())
+					Expect(variable.Ref).To(Equal(tt.ref))
+					Expect(variable.Value).To(Equal(tt.val))
+				} else {
+					Expect(err).To(MatchError(tt.err))
+				}
+			})
+		}
+	})
+})

tdmtrader/jetbridge

fly/commands/internal/flaghelpers/yaml_variable_pair_flag_test.go
@@ -1,0 +1,65 @@
+package flaghelpers_test
+
+import (
+	"encoding/json"
+
+	"github.com/concourse/concourse/vars"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/concourse/concourse/fly/commands/internal/flaghelpers"
+)
+
+var _ = Describe("YAMLVariablePair", func() {
+	Describe("UnmarshalFlag", func() {
+		var variable *flaghelpers.YAMLVariablePairFlag
+
+		BeforeEach(func() {
+			variable = &flaghelpers.YAMLVariablePairFlag{}
+		})
+
+		for _, tt := range []struct {
+			desc string
+			flag string
+			ref  vars.VariableReference
+			val  interface{}
+			err  string
+		}{
+			{
+				desc: "basic",
+				flag: "key=value",
+				ref:  vars.VariableReference{Path: "key", Fields: []string{}},
+				val:  "value",
+			},
+			{
+				desc: "unmarshals arbitrary yaml",
+				flag: `key={hello:world: [a, b, c]}`,
+				ref:  vars.VariableReference{Path: `key`, Fields: []string{}},
+				val:  map[string]interface{}{"hello:world": []interface{}{"a", "b", "c"}},
+			},
+			{
+				desc: "unmarshals numbers as json.Number",
+				flag: `key={int: 1, float: 1.23}`,
+				ref:  vars.VariableReference{Path: `key`, Fields: []string{}},
+				val:  map[string]interface{}{"int": json.Number("1"), "float": json.Number("1.23")},
+			},
+			{
+				desc: "errors if yaml is malformed",
+				flag: `key={a: b`,
+				err:  `error converting YAML to JSON: yaml: line 1: did not find expected ',' or '}'`,
+			},
+		} {
+			tt := tt
+			It(tt.desc, func() {
+				err := variable.UnmarshalFlag(tt.flag)
+				if tt.err == "" {
+					Expect(err).ToNot(HaveOccurred())
+					Expect(variable.Ref).To(Equal(tt.ref))
+					Expect(variable.Value).To(Equal(tt.val))
+				} else {
+					Expect(err).To(MatchError(tt.err))
+				}
+			})
+		}
+	})
+})