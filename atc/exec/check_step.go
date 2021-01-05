cocornd/concourse

atc: structure: remove logger from worker.Client
use lagerctx instead

Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

atc/exec/check_step.go
@@ -296,8 +296,7 @@ func (step *CheckStep) runCheck(
 	}
 
 	return step.workerClient.RunCheckStep(
-		ctx,
-		logger,
+		lagerctx.NewContext(ctx, logger),
 		step.containerOwner(resourceConfig),
 		containerSpec,
 		workerSpec,

tdmtrader/jetbridge

atc/exec/check_step.go
@@ -307,8 +307,7 @@ func (step *CheckStep) runCheck(
 	defer cancel()
 
 	return step.workerClient.RunCheckStep(
-		processCtx,
-		logger,
+		lagerctx.NewContext(processCtx, logger),
 		step.containerOwner(resourceConfig),
 		containerSpec,
 		workerSpec,