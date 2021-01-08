concourse/concourse

fly(fix): set-pipeline promoted unpause-pipeline command should have --team option.
Signed-off-by: Chao Li <chaol@vmware.com>

fly/commands/internal/setpipelinehelpers/atc_config_test.go
@@ -42,6 +42,20 @@ var _ = Describe("UnpausePipelineCommand", func() {
 		Expect(atcConfig.UnpausePipelineCommand()).To(Equal(expected))
 	})
 
+	Context("when given a different team name", func() {
+		It("adds --team option", func() {
+			atcConfig := ATCConfig{
+				TargetName: "my-target",
+				PipelineRef: atc.PipelineRef{
+					Name: "my-pipeline",
+				},
+				GivenTeamName: "other-team",
+			}
+			expected := fmt.Sprintf(`%s -t my-target unpause-pipeline -p my-pipeline --team other-team`, os.Args[0])
+			Expect(atcConfig.UnpausePipelineCommand()).To(Equal(expected))
+		})
+	})
+
 	Context("when the instance vars require quoting", func() {
 		It("quotes the pipeline flag", func() {
 			atcConfig := ATCConfig{

tdmtrader/jetbridge

Conflicts:
	fly/commands/internal/setpipelinehelpers/atc_config.go
	fly/commands/internal/setpipelinehelpers/atc_config_test.go

fly/commands/internal/setpipelinehelpers/atc_config_test.go
@@ -37,4 +37,16 @@ var _ = Describe("UnpausePipelineCommand", func() {
 		expected := fmt.Sprintf("%s -t my-target unpause-pipeline -p my-pipeline", os.Args[0])
 		Expect(atcConfig.UnpausePipelineCommand()).To(Equal(expected))
 	})
+
+	Context("when given a different team name", func() {
+		It("adds --team option", func() {
+			atcConfig := ATCConfig{
+				TargetName: "my-target",
+				PipelineName: "my-pipeline",
+				GivenTeamName: "other-team",
+			}
+			expected := fmt.Sprintf(`%s -t my-target unpause-pipeline -p my-pipeline --team other-team`, os.Args[0])
+			Expect(atcConfig.UnpausePipelineCommand()).To(Equal(expected))
+		})
+	})
 })