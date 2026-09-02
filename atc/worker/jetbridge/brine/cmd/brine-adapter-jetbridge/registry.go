90786cf54094e05f935cab99c7e3c840e04f0638
Thomas Moore <tdmtrader@Mac.localdomain>
2026-09-02 21:38:02 +0000

    test: prepare final strict db team worker cohort

--- atc/worker/jetbridge/brine/cmd/brine-adapter-jetbridge/registry.go
+++ atc/worker/jetbridge/brine/cmd/brine-adapter-jetbridge/registry.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"path/filepath"
+
 	"github.com/brine-dev/brine-go/pkg/brine"
 	"github.com/concourse/concourse/atc/worker/jetbridge/brine/steps"
 )
@@ -9,6 +11,17 @@ func buildAppRegistry() brine.StepRegistry {
 	return brine.NewStepRegistry(steps.Definitions())
 }
 
-func buildAppResources() (*brine.ResourceRegistry, error) {
-	return brine.NewResourceRegistry(steps.ResourceDefinitions())
+func buildAppResources(args []string) (*brine.ResourceRegistry, error) {
+	definitions := steps.ResourceDefinitions()
+	features, _, _ := parseRunFlags(args)
+	if len(features) == 1 && filepath.Base(features[0]) == "db-team-remaining-strict.feature" {
+		productionDBOnly := make([]brine.ResourceDefinition, 0, 2)
+		for _, definition := range definitions {
+			if definition.Name == "postgres" || definition.Name == "jetbridge-db" {
+				productionDBOnly = append(productionDBOnly, definition)
+			}
+		}
+		definitions = productionDBOnly
+	}
+	return brine.NewResourceRegistry(definitions)
 }
