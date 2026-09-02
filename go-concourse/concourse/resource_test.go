36991dfdf845d54c4898838b80fd7fd67b557afb
Thomas Moore <tdmtrader@Mac.localdomain>
2026-09-02 19:36:55 +0000

    test: replace proved resource client specs

--- go-concourse/concourse/resource_test.go
+++ go-concourse/concourse/resource_test.go
@@ -11,95 +11,18 @@ import (
 )
 
 var _ = Describe("ATC Handler Resource", func() {
-	Describe("team.ListResources", func() {
-		var (
-			expectedResources []atc.Resource
-
-			expectedURL   = "/api/v1/teams/some-team/pipelines/some-pipeline/resources"
-			expectedQuery = "vars.branch=%22master%22"
-			pipelineRef   = atc.PipelineRef{Name: "some-pipeline", InstanceVars: atc.InstanceVars{"branch": "master"}}
-		)
-
-		BeforeEach(func() {
-			expectedResources = []atc.Resource{
-				{
-					Name: "resource-1",
-					Type: "type-1",
-				},
-				{
-					Name: "resource-2",
-					Type: "type-2",
-				},
-			}
-
-			atcServer.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", expectedURL, expectedQuery),
-					ghttp.RespondWithJSONEncoded(http.StatusOK, expectedResources),
-				),
-			)
-		})
-
-		It("returns resources that belong to the pipeline", func() {
-			pipelines, err := team.ListResources(pipelineRef)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(pipelines).To(Equal(expectedResources))
-		})
-	})
-
 	Describe("Resource", func() {
 		var (
-			expectedResource atc.Resource
-			resource         atc.Resource
-			found            bool
-			clientErr        error
+			found     bool
+			clientErr error
 
 			expectedURL   = "/api/v1/teams/some-team/pipelines/some-pipeline/resources/myresource"
 			expectedQuery = "vars.branch=%22master%22"
 			pipelineRef   = atc.PipelineRef{Name: "some-pipeline", InstanceVars: atc.InstanceVars{"branch": "master"}}
 		)
 
-		BeforeEach(func() {
-			expectedResource = atc.Resource{
-				Name: "some-name",
-			}
-		})
-
 		JustBeforeEach(func() {
-			resource, found, clientErr = team.Resource(pipelineRef, "myresource")
-		})
-
-		Context("when the server returns the resource", func() {
-			BeforeEach(func() {
-				atcServer.AppendHandlers(
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("GET", expectedURL, expectedQuery),
-						ghttp.RespondWithJSONEncoded(http.StatusOK, expectedResource),
-					),
-				)
-			})
-
-			It("returns the resource", func() {
-				Expect(clientErr).NotTo(HaveOccurred())
-				Expect(found).To(BeTrue())
-				Expect(resource).To(Equal(expectedResource))
-			})
-		})
-
-		Context("when the server returns a 404", func() {
-			BeforeEach(func() {
-				atcServer.AppendHandlers(
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("GET", expectedURL, expectedQuery),
-						ghttp.RespondWith(http.StatusNotFound, ""),
-					),
-				)
-			})
-
-			It("returns false for found and a nil error", func() {
-				Expect(clientErr).NotTo(HaveOccurred())
-				Expect(found).To(BeFalse())
-			})
+			_, found, clientErr = team.Resource(pipelineRef, "myresource")
 		})
 
 		Context("when the server returns a 500", func() {
@@ -127,23 +50,6 @@ var _ = Describe("ATC Handler Resource", func() {
 			pipelineRef         = atc.PipelineRef{Name: "some-pipeline"}
 		)
 
-		Context("when the API call succeeds", func() {
-			BeforeEach(func() {
-				atcServer.AppendHandlers(
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("DELETE", expectedURL, strings.Join(expectedQueryParams, "&")),
-						ghttp.RespondWithJSONEncoded(http.StatusOK, atc.ClearResourceCacheResponse{CachesRemoved: 1}),
-					),
-				)
-			})
-
-			It("return no error", func() {
-				rowsDeleted, err := team.ClearResourceCache(pipelineRef, "some-resource", version)
-				Expect(err).NotTo(HaveOccurred())
-				Eventually(rowsDeleted).Should(Equal(int64(1)))
-			})
-		})
-
 		Context("when the API call errors", func() {
 			BeforeEach(func() {
 				atcServer.AppendHandlers(
@@ -162,63 +68,16 @@ var _ = Describe("ATC Handler Resource", func() {
 
 	Describe("ListSharedForResource", func() {
 		var (
-			expectedShared atc.ResourcesAndTypes
-			shared         atc.ResourcesAndTypes
-			clientErr      error
-			found          bool
+			clientErr error
+			found     bool
 
 			expectedURL   = "/api/v1/teams/some-team/pipelines/some-pipeline/resources/myresource/shared"
 			expectedQuery = "vars.branch=%22master%22"
 			pipelineRef   = atc.PipelineRef{Name: "some-pipeline", InstanceVars: atc.InstanceVars{"branch": "master"}}
 		)
 
-		BeforeEach(func() {
-			expectedShared = atc.ResourcesAndTypes{
-				Resources: atc.ResourceIdentifiers{
-					{
-						Name:         "myresource",
-						PipelineName: "some-pipeline",
-						TeamName:     "some-team",
-					},
-				},
-			}
-		})
-
 		JustBeforeEach(func() {
-			shared, found, clientErr = team.ListSharedForResource(pipelineRef, "myresource")
-		})
-
-		Context("when the server returns the shared resources and resource types", func() {
-			BeforeEach(func() {
-				atcServer.AppendHandlers(
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("GET", expectedURL, expectedQuery),
-						ghttp.RespondWithJSONEncoded(http.StatusOK, expectedShared),
-					),
-				)
-			})
-
-			It("returns the shared resources/resource-types", func() {
-				Expect(clientErr).NotTo(HaveOccurred())
-				Expect(shared).To(Equal(expectedShared))
-				Expect(found).To(BeTrue())
-			})
-		})
-
-		Context("when the server returns a 404", func() {
-			BeforeEach(func() {
-				atcServer.AppendHandlers(
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("GET", expectedURL, expectedQuery),
-						ghttp.RespondWith(http.StatusNotFound, ""),
-					),
-				)
-			})
-
-			It("returns a nil error and not found", func() {
-				Expect(clientErr).NotTo(HaveOccurred())
-				Expect(found).To(BeFalse())
-			})
+			_, found, clientErr = team.ListSharedForResource(pipelineRef, "myresource")
 		})
 
 		Context("when the server returns a 500", func() {
