1e1d144facb57ee9a59c789af3f11a5de5399ecb
Thomas Moore <tdmtrader@Mac.localdomain>
2026-09-02 20:32:41 +0000

    test: prepare strict pipeline factory migration

--- atc/worker/jetbridge/brine/steps/registry.go
+++ atc/worker/jetbridge/brine/steps/registry.go
@@ -179,6 +179,7 @@ func Definitions() []brine.StepDefinition {
 	defs = append(defs, DBVersionsFinalStrictDefinitions()...)
 	defs = append(defs, DBResourceConfigCheckSessionLifecycleStrictDefinitions()...)
 	defs = append(defs, DBResourceConfigFactoryStrictDefinitions()...)
+	defs = append(defs, DBPipelineFactoryStrictDefinitions()...)
 	return defs
 }
 
