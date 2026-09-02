7a46becb0e904c568fb848d3fb62f5f53c2cfd2c
Thomas Moore <tdmtrader@Mac.localdomain>
2026-09-02 21:08:42 +0000

    test: add final strict resource config cohort

--- atc/worker/jetbridge/brine/steps/registry.go
+++ atc/worker/jetbridge/brine/steps/registry.go
@@ -184,6 +184,7 @@ func Definitions() []brine.StepDefinition {
 	defs = append(defs, DBVersionsFinalStrictDefinitions()...)
 	defs = append(defs, DBResourceConfigCheckSessionLifecycleStrictDefinitions()...)
 	defs = append(defs, DBResourceConfigFactoryStrictDefinitions()...)
+	defs = append(defs, DBResourceConfigFinalStrictDefinitions()...)
 	defs = append(defs, DBPipelineFactoryStrictDefinitions()...)
 	defs = append(defs, DBPipelinePauserStrictDefinitions()...)
 	defs = append(defs, IDTokenManagerStrictDefinitions()...)
