e58b4d1369e89777544d1485e7eaf93f4008d34c
Thomas Moore <tdmtrader@Mac.localdomain>
2026-09-02 21:41:56 +0000

    test: remove paired build events bigint migration specs

--- atc/db/migration/build_events_bigint_test.go
+++ atc/db/migration/build_events_bigint_test.go
@@ -2,7 +2,6 @@ package migration_test
 
 import (
 	"database/sql"
-	"fmt"
 	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -67,71 +66,6 @@ var _ = Describe("build_events bigint indexes", func() {
 			Expect(plan).To(ContainSubstring("Index Scan"))
 		})
 
-		It("has indexes for both build_id_old and build_id in pipeline partitions", func() {
-			plan := explain(fmt.Sprintf(
-				`SELECT * FROM pipeline_build_events_%d WHERE build_id = 1`,
-				pipelineID,
-			))
-			Expect(plan).To(ContainSubstring("Index Scan"))
-
-			plan = explain(fmt.Sprintf(
-				`SELECT * FROM pipeline_build_events_%d WHERE build_id_old = 1`,
-				pipelineID,
-			))
-			Expect(plan).To(ContainSubstring("Index Scan"))
-		})
-
-		It("has indexes for only build_id in team partitions", func() {
-			plan := explain(fmt.Sprintf(
-				`SELECT * FROM team_build_events_%d WHERE build_id = 1`,
-				teamID,
-			))
-			Expect(plan).To(ContainSubstring("Index Scan"))
-
-			plan = explain(fmt.Sprintf(
-				`SELECT * FROM team_build_events_%d WHERE build_id_old = 1`,
-				pipelineID,
-			))
-			Expect(plan).To(ContainSubstring("Seq Scan"))
-		})
-
-		It("has indexes for newly created team partitions", func() {
-			var newTeamID int
-			err := db.QueryRow(`
-				INSERT INTO teams (name, auth)
-				VALUES ('some-other-team', '{}')
-				RETURNING id
-			`).Scan(&newTeamID)
-			Expect(err).ToNot(HaveOccurred())
-
-			plan := explain(fmt.Sprintf(
-				`SELECT * FROM team_build_events_%d WHERE build_id = 1`,
-				newTeamID,
-			))
-			Expect(plan).To(ContainSubstring("Index Scan"))
-		})
-
-		It("has indexes for both build_id and build_id_old in newly created pipeline partitions", func() {
-			var newPipelineID int
-			err := db.QueryRow(`
-				INSERT INTO pipelines (name, team_id)
-				VALUES ('some-other-pipeline', $1)
-				RETURNING id
-			`, teamID).Scan(&newPipelineID)
-			Expect(err).ToNot(HaveOccurred())
-
-			plan := explain(fmt.Sprintf(
-				`SELECT * FROM pipeline_build_events_%d WHERE build_id = 1`,
-				newPipelineID,
-			))
-			Expect(plan).To(ContainSubstring("Index Scan"))
-
-			plan = explain(fmt.Sprintf(
-				`SELECT * FROM pipeline_build_events_%d WHERE build_id_old = 1`,
-				newPipelineID,
-			))
-			Expect(plan).To(ContainSubstring("Index Scan"))
-		})
 	})
 
 	Describe("Down", func() {
@@ -145,60 +79,5 @@ var _ = Describe("build_events bigint indexes", func() {
 			Expect(plan).ToNot(ContainSubstring("build_id_old"))
 		})
 
-		It("has index for build_id_old in pipeline partitions", func() {
-			plan := explain(fmt.Sprintf(
-				`SELECT * FROM pipeline_build_events_%d WHERE build_id_old = 1`,
-				pipelineID,
-			))
-			Expect(plan).To(ContainSubstring("Index Scan"))
-		})
-
-		It("does not have index for build_id in pipeline partitions", func() {
-			plan := explain(fmt.Sprintf(
-				`SELECT * FROM pipeline_build_events_%d WHERE build_id = 1`,
-				pipelineID,
-			))
-			Expect(plan).To(ContainSubstring("Seq Scan"))
-		})
-
-		It("does not have index for build_id in team partitions", func() {
-			plan := explain(fmt.Sprintf(
-				`SELECT * FROM team_build_events_%d WHERE build_id = 1`,
-				teamID,
-			))
-			Expect(plan).To(ContainSubstring("Seq Scan"))
-		})
-
-		It("does not have index for build_id in newly created team partitions", func() {
-			var newTeamID int
-			err := db.QueryRow(`
-				INSERT INTO teams (name, auth)
-				VALUES ('some-other-team', '{}')
-				RETURNING id
-			`).Scan(&newTeamID)
-			Expect(err).ToNot(HaveOccurred())
-
-			plan := explain(fmt.Sprintf(
-				`SELECT * FROM team_build_events_%d WHERE build_id = 1`,
-				newTeamID,
-			))
-			Expect(plan).To(ContainSubstring("Seq Scan"))
-		})
-
-		It("has index for build_id in newly created pipeline partitions", func() {
-			var newPipelineID int
-			err := db.QueryRow(`
-				INSERT INTO pipelines (name, team_id)
-				VALUES ('some-other-pipeline', $1)
-				RETURNING id
-			`, teamID).Scan(&newPipelineID)
-			Expect(err).ToNot(HaveOccurred())
-
-			plan := explain(fmt.Sprintf(
-				`SELECT * FROM pipeline_build_events_%d WHERE build_id = 1`,
-				newPipelineID,
-			))
-			Expect(plan).To(ContainSubstring("Index Scan"))
-		})
 	})
 })
