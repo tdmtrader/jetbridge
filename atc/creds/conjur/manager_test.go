64fad7c0df99f0a85a2cadd015688c154ac6a337
Thomas Moore <tdmtrader@Mac.localdomain>
2026-09-02 21:15:17 +0000

    test: remove paired Conjur manager specs

--- atc/creds/conjur/manager_test.go
+++ atc/creds/conjur/manager_test.go
@@ -11,22 +11,6 @@ import (
 var _ = Describe("Manager", func() {
 	var manager conjur.Manager
 
-	Describe("IsConfigured()", func() {
-		JustBeforeEach(func() {
-			_, err := flags.ParseArgs(&manager, []string{})
-			Expect(err).To(BeNil())
-		})
-
-		It("fails on empty Manager", func() {
-			Expect(manager.IsConfigured()).To(BeFalse())
-		})
-
-		It("passes if ConjurApplianceURL is set", func() {
-			manager.ConjurApplianceUrl = "http://conjur-test"
-			Expect(manager.IsConfigured()).To(BeTrue())
-		})
-	})
-
 	Describe("Validate()", func() {
 		JustBeforeEach(func() {
 			manager = conjur.Manager{
